b439777aae1ce0013418fd5ff48c66dd73589ad4
AMorad09 <ssvz4436@leeds.ac.uk>
2025-10-07 19:49:06 +0000

    finished task252

--- tasks/task2_5_2/task252.kt
+++ tasks/task2_5_2/task252.kt
@@ -1,10 +1,17 @@
 fun main() {
 
-val myAge: = 29u
-val universeAge = 13_800_000_000L
-val status = 'M'
-val name = "Sarah"
-val height = 1.78f
-val root2 = Math.sqrt(2.0)
+val myAge: UInt = 29u
+val universeAge: Long = 13_800_000_000L
+val status: Char = 'M'
+val name: String = "Sarah"
+val height: Float = 1.78f
+val root2: Double = Math.sqrt(2.0)
+
+println(myAge)
+println(universeAge)
+println(status)
+println(name)
+println(height)
+println(root2)
 
 }
