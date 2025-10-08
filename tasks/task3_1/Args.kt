0a8d10302df03ff27102cc873adb38c65a693e9f
AMorad09 <ssvz4436@leeds.ac.uk>
2025-10-08 12:00:33 +0000

    extra work

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,14 @@
 // Task 3.1: command line arguments
+
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size != 2) {
+        println("Error: amounts of arguments not met")
+        exitProcess(1)
+    }
+    println(args[0])
+    println(args[1])
+    }
+
+    // required argument available as args[0]
