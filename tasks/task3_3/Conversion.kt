a0ceb43fb727fbe630d9b8a965a6b63549bcf7f9
AMorad09 <ssvz4436@leeds.ac.uk>
2025-10-11 13:56:32 +0000

    finished 3.

--- tasks/task3_3/Conversion.kt
+++ tasks/task3_3/Conversion.kt
@@ -1,1 +1,6 @@
-// Task 3.3: conversion of strings to numbers
+// Task 3.3: conversion of strings to number
+fun main() {
+    print("Please enter your age: ")
+    val age = readln().toDoubleOrNull()
+    println(age)
+}
